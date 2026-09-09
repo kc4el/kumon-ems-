f5b29babae61c794e505aaed994bf7cc2e503714
YuzuAoi-debug <h3yf00lxd@gmail.com>
2026-09-10 00:31:21 +0800

    Update urlpatterns to include static and media handling
    Added static file serving and media URL handling for debug mode.

--- kumon_ems/urls.py
+++ kumon_ems/urls.py
@@ -22,4 +22,7 @@ urlpatterns += [
     re_path(r'^static/(?P<path>.*)$', serve, {'insecure': True}),
 ]
 
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
